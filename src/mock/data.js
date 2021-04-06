78eedf0b04348a7432d0b9ac49656f2aa77d8fb7
retfloww <67190487+retfloww@users.noreply.github.com>
2021-04-06 09:28:53 -0700

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,7 +30,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'QuGuesser.png',
     title: 'QuGuesser',
-    info: 'QuGuesser ist ein Spiel wo du in eine zufällige Google Street View reinversetzt wirst. Deine Aufgabe ist es den Ort zu erraten.',
+    info: 'QuGuesser ist ein Spiel wo du in eine zufällige Google Street View reinversetzt wirst. Deine Aufgabe ist es den Ort zu erraten',
     info2: '',
     url: 'http://161.97.137.13:3000/',
     repo: 'https://github.com/Rvvlf/QuGuesser', // if no repo, the button will not show up
@@ -39,7 +39,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'QuOnline.png',
     title: 'QuOnline',
-    info: 'QuOnline ist ein online Filehosting-Dienst. Die Plattform ermöglicht es Datein hochzuladen, herunterzuladen oder zu löschen. Backend basiert auf NodeJS und GoLang. Veränderungen werden dynamisch von Javascript übernommen.',
+    info: 'QuOnline ist ein online Filehosting-Dienst. Die Plattform ermöglicht es Datein hochzuladen, herunterzuladen oder zu löschen. Backend basiert auf NodeJS und GoLang. Veränderungen werden dynamisch von Javascript übernommen',
     info2: '',
     url: '',
     repo: 'https://github.com/Rvvlf/QuOnline', // if no repo, the button will not show up
@@ -48,7 +48,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'QuChat.png',
     title: 'QuChat',
-    info: 'QuChat ist eine Chat App die auf NodeJS basiert. Als Protokoll wird WebRTC benutzt.',
+    info: 'QuChat ist eine Chat App die auf NodeJS basiert. Als Protokoll wird WebRTC benutzt',
     info2: '',
     url: 'https://www.dropbox.com/s/glw4w4xeu6m1lan/Fg45Sdf.mp4?dl=0',
     repo: 'https://github.com/Rvvlf/QuChat', // if no repo, the button will not show up
@@ -61,6 +61,15 @@ export const projectsData = [
     info2: '',
     url: 'https://www.dropbox.com/s/jl64rc38pn0ier6/Fghc%403zx.mp4?dl=0',
     repo: 'https://github.com/Rvvlf/QuDrive', // if no repo, the button will not show up
+  },
+    {
+    id: nanoid(),
+    img: 'quepeeker.png',
+    title: 'QuePeeker',
+    info: 'QuePeeker ist eine Software für die Fernsteuerung, dass auf C++ QT Framework und die Windows32API basiert',
+    info2: '',
+    url: 'https://www.dropbox.com/s/t5q6hwqprexp2t0/qe%23fp4ee%24k.mp4?dl=0',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
